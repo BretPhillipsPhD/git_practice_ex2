d2a0315796795efc0006ac8bf8621f7f8f0a0594
lauren-h-fulton <149395142+lauren-h-fulton@users.noreply.github.com>
2023-11-01 15:19:29 -0400

    replaced base R histograms with ggplot histograms

--- server.R
+++ server.R
@@ -1,6 +1,8 @@
+library(ggplot2)
+
 # Define server logic required to draw a histogram ----
 server <- function(input, output) {
-  
+
   # Histogram of the Old Faithful Geyser Data ----
   # with requested number of bins
   # This expression that generates a histogram is wrapped in a call
@@ -10,20 +12,35 @@ server <- function(input, output) {
   #    re-executed when inputs (input$bins) change
   # 2. Its output type is a plot
   output$geyserPlot <- renderPlot({
-    
-    x <- faithful$waiting
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
-    
-    hist(x,
-         breaks = bins,
-         # allow input from the bar color dropdown
-         col = input$colors,
-         border = "white",
-         xlab = "Waiting time to next eruption (in mins)",
-         labels = as.logical(input$labels),
-         main = "Histogram of waiting times"
-    )
-    
+
+    bins <- seq(min(faithful$waiting), max(faithful$waiting), length.out = input$bins + 1)
+
+    if(input$labels == TRUE) {
+      ggplot(data = faithful, aes(y = waiting)) +
+        geom_histogram(breaks = bins,
+                       fill = input$colors,
+                       col = "white") +
+        theme_classic() +
+        scale_x_continuous(expand = c(0,0)) +
+        scale_y_continuous(expand = c(0,0)) +
+        stat_bin(aes(label = ..count..),
+                 breaks = bins,
+                 geom = "text",
+                 color = "white",
+                 position = position_stack(vjust = 0.8)
+                 )
+    }
+
+    else {
+      ggplot(data = faithful, aes(y = waiting)) +
+        geom_histogram(breaks = bins,
+                       fill = input$colors,
+                       col = "white") +
+        theme_classic() +
+        scale_x_continuous(expand = c(0,0)) +
+        scale_y_continuous(expand = c(0,0))
+    }
+
   })
-  
+
 }
