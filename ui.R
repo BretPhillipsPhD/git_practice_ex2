693c9bce0ab353124dc7ab14158a31b40b805d13
lauren-h-fulton <149395142+lauren-h-fulton@users.noreply.github.com>
2023-11-01 15:20:32 -0400

    replaced base R histograms with ggplot histograms

--- ui.R
+++ ui.R
@@ -1,24 +1,25 @@
 library(shiny)
+library(ggplot2)
 
 # Define UI for app that draws a histogram ----
 ui <- fluidPage(
-  
+
   # App title ----
   titlePanel("Hello Shiny!"),
-  
+
   # Sidebar layout with input and output definitions ----
   sidebarLayout(
-    
+
     # Sidebar panel for inputs ----
     sidebarPanel(
-      
+
       # Input: Slider for the number of bins ----
       sliderInput(inputId = "bins",
                   label = "Number of bins:",
                   min = 1,
                   max = 50,
                   value = 30),
-      
+
       # Input: Dropdown for the bar colors ----
       selectInput(inputId = "colors",
                   label = "Bar color:",
@@ -29,7 +30,7 @@ ui <- fluidPage(
                   ),
                   selected = "blue"
       ),
-      
+
       # Input: Radio buttons for count display ----
       radioButtons(inputId = "labels",
                    label = "Display count",
@@ -39,14 +40,14 @@ ui <- fluidPage(
                    ),
                    selected = FALSE
       )
-      
+
     ),
     # Main panel for displaying outputs ----
     mainPanel(
-      
+
       # Output: Histogram ----
       plotOutput(outputId = "geyserPlot")
-      
+
     )
   )
 )
